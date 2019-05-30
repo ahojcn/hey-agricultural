52ce8ef1c4b30df1b3e55407101fe6a7b104cce7
ahojcn <hanoi_ahoj@icloud.com>
2019-05-30 13:27:49 +0800

    nothing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/View/Layout/HelloWorld'
 
 import index from '@/View/Layout/index' // 首页
 
