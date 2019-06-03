a75fe35f0aaa5d498d2b784b23cef32285820518
ahojcn <hanoi_ahoj@icloud.com>
2019-06-03 17:30:23 +0800

    test server

--- src/main.js
+++ src/main.js
@@ -27,9 +27,9 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 // Vue.http.options.root = 'http://106.12.202.93:8080';
-// Vue.http.options.root = 'http://106.12.202.93:8080';
+Vue.http.options.root = 'http://106.12.202.93:8080';
 // 开发环境服务器
-Vue.http.options.root = 'http://tim.natapp1.cc';
+// Vue.http.options.root = 'http://tim.natapp1.cc';
 Vue.http.interceptors.push(function (request, next) {//拦截器
 // 跨域携带cookie
   request.credentials = true;
